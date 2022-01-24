6bb40e1a1bdbf810cad131078083a6cf674cf57f
Brendan Cheong <brendancej82@gmail.com>
2022-01-24 11:29:35 +0000

    [LeetCode Sync] Runtime - 12 ms (100.00%), Memory - 10.1 MB (100.00%)

---
+++ 0215-kth-largest-element-in-an-array/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - k];
+    }
+};
