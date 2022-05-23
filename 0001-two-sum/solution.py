59ae0e2d197a47478d2358f7dcb8496d5db3db5d
Brendan Cheong <brendancej82@gmail.com>
2022-05-23 14:00:10 +0000

    [LeetCode Sync] Runtime - 114 ms (41.38%), Memory - 15.2 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,10 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Time Complexity: O(n), with O(1 + α) amortized HashTable operation
+        Explanation: Each element has its leftover amount given the target
+        like for example, given target 9, and element 2 will have leftover 7
+        and element 7 will have leftover 2.
+        
+        When there is a pair of leftovers that are inside the array, we have an answer.
+        We can store each {ele: leftover} and check to see if the leftover is in the HashTable
+        to create this pair, for each element.
+        """
         dct = dict()
-        for ele in range(0, len(nums)):
-            complement = target - nums[ele]
-            if (complement in dct):
-                return [dct[complement], ele]
+        for i in range(0, len(nums)):
+            leftover = target - nums[i]
+            if (leftover in dct):
+                return [dct[leftover], i]
             else:
-                dct[nums[ele]] = ele 
+                dct[nums[i]] = i
+        return false
         
