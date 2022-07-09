34d14e1240bcf91bc30393a008aa15f9a0a730e6
Brendan Cheong <brendancej82@gmail.com>
2022-07-09 02:46:16 +0000

    [LeetCode Sync] Runtime - 144 ms (5.20%), Memory - 18.2 MB (100.00%)

--- 0056-merge-intervals/solution.py
+++ 0056-merge-intervals/solution.py
@@ -4,15 +4,24 @@ class Solution:
         Time: O(n log n), we must sort based on start value
         
         """
-        intervals = sorted(intervals, key=lambda arr: arr[0])
-        res = [intervals[0]]
-        
-        for start, end in intervals[1:]:
-            lastEnd = res[-1][-1]
-            
-            if start <= lastEnd:
-                res[-1][-1] = max(lastEnd, end)
+        START, END = 0, -1
+        intervals = sorted(intervals, key=lambda int: int[START])
+        # usually we can assume that its sorted according to start time, but in this case its not lol.
+        res = []
+        for i, curr_int in enumerate(intervals):
+            prev = res[END] if res else None
+            # edge cases:
+            # new interval goes before all the intervals, no merging
+            # new interval goes after all the intervals, no merging
+            if (i == 0):
+                # edge case where there is only 1 interval
+                res.append(curr_int)
+            elif (curr_int[START] <= prev[END]):
+                # merge when there is an overlap
+                merged_interval = [min(curr_int[START], prev[START]), max(curr_int[END], prev[END])]
+                res.pop() and res.append(merged_interval)
             else:
-                res.append([start, end])
+                # if there is no overlap, just append
+                res.append(curr_int)
         return res
         
