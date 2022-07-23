dc6c93dac9652c0ae94bf3df0feb581d8d3bd46d
Brendan Cheong <brendancej82@gmail.com>
2022-07-23 10:13:39 +0000

    [LeetCode Sync] Runtime - 34 ms (88.12%), Memory - 14 MB (100.00%)

---
+++ 0139-word-break/solution.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        q = deque([s])
+        seen = set() 
+        while q:
+            s = q.popleft()    # popleft() = BFS ; pop() = DFS
+            for word in wordDict:
+                if s.startswith(word):
+                    new_s = s[len(word):]
+                    if new_s == "": 
+                        return True
+                    if new_s not in seen:
+                        q.append(new_s)
+                        seen.add(new_s)
+        return False
