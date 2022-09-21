e2f26a15ae079f12909ef132ab26f316086db27a
Brendan Cheong <brendancej82@gmail.com>
2022-09-21 21:28:52 +0000

    [LeetCode Sync] Runtime - 109 ms (5.13%), Memory - 14.4 MB (100.00%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,18 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        """
-        O(n) loop, only one pass, with O(1) memory / space complexity
-        """
-        l, r = 0, len(s) - 1
-        while l < r:
-            if not s[l].isalnum():
-                l += 1
-            elif not s[r].isalnum():
-                r -= 1
-            else:
-                if s[l].lower() != s[r].lower():
-                    return False
-                else:
-                    l += 1
-                    r -= 1
+        # do the 2 pointers technique
+        # this is a O(1) space technique
+        # With a O(n) time
+        start = 0
+        end = len(s) - 1
+        while start < end: # iterate until start passes the end
+            while not s[start].isalnum() and start < end:
+                # skip spaces/non-alphanumeric characters
+                # do not execute if the pointers pass each other (edge case)
+                start += 1 # move pointer towards end
+            while not s[end].isalnum() and start < end:
+                # same idea here
+                end -= 1 # move pointer to the start
+            # if statement must come first to check edge case of beginning letter
+            # account for edge case of lowercased letters.
+            # do not do s = s.lower() as its O(N) time
+            if (s[start].lower() != s[end].lower()):
+                return False
+            start += 1
+            end -= 1
         return True
+        
+        
