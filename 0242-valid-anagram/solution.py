079a0b0c04f48510e3b83d4e1512e72894444fa6
Brendan Cheong <brendancej82@gmail.com>
2022-10-12 20:54:00 +0000

    [LeetCode Sync] Runtime - 110 ms (5.24%), Memory - 14.5 MB (100.00%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,8 +1,18 @@
-from collections import Counter
+from collections import defaultdict
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count1 = Counter(s)
-        count2 = Counter(t)
-        return count1 == count2
-        
+        """
+        you can use a counter, or in this case you can
+        use just one dictionary, then decrease the count of each letter
+        if the letter count is not 0, its not an anagram
+        """
+        dct = defaultdict(int)
+        for i, ele in enumerate(s):
+            dct[ele] += 1
+        for i, ele in enumerate(t):
+            dct[ele] -= 1
+        for k, v in dct.items():
+            if (v != 0):
+                return False
+        return True
