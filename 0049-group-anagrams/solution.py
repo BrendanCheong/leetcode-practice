80c6b1882fcdea563bc7421bebb3734f0a4da714
Brendan Cheong <brendancej82@gmail.com>
2022-10-12 21:26:53 +0000

    [LeetCode Sync] Runtime - 289 ms (5.07%), Memory - 19.9 MB (65.63%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,16 +1,32 @@
-from collections import Counter
 from collections import defaultdict
 import string
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for word in strs:
-            alphabet = dict.fromkeys(string.ascii_lowercase, 0)
-            for char in word:
-                alphabet[char] += 1
-            key = tuple(alphabet.items())
-            res[key].append(word)
+        """
+        Naive solution would be to create a dictionary for each word
+        then O(n^2) loop, for each word find the matching dictionary with
+        the same value
+       
+        Another idea is to sort the words, but its gonna take N log N for M times which is M = length of array
+        
+        The trick is that in order to find matching words in O(1) time, we have to use tuples!!! and HashMaps. So each word has its own tuple (which is the character count) and we can match the word(s) to its tuple(s)
+        
+        This is O(m * n) time, m number of words, n length of each word
+        space is O(n)
+        """
+        res = defaultdict(list) # dct of character count arrays
+        
+        for i, word in enumerate(strs):
+            char_arr = [0] * 26 # create blank char arr for each word
+            for j in word:
+                char_arr[ord(j) - ord('a')] += 1
+            # now that we have our char arr we can add it to our dict
+            # have to use tuple to make it unique
+            # add the corresponding word to char_arr
+            res[tuple(char_arr)].append(word)
+        # only return the values with the array
         return res.values()
+            
                 
         
