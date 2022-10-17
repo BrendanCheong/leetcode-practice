83c72ed5929a2f628f9f5fb6430918fb1b934566
Brendan Cheong <brendancej82@gmail.com>
2022-10-17 00:11:08 +0000

    [LeetCode Sync] Runtime - 51 ms (6.27%), Memory - 20.6 MB (5.77%)

--- 0206-reverse-linked-list/solution.py
+++ 0206-reverse-linked-list/solution.py
@@ -5,13 +5,34 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None # prev is just to keep track of previously visited nodes to keep track of
-        curr = head
-        while (curr):
-            next = curr.next
-            curr.next = prev # make the node point to the previous node, starting from None
-            prev = curr # keep track of previous node
-            curr = next # move on to original next val
-        return prev
+        """
+        We want the current head to point to none
+        then the next node point to the previous node
+        do until the last node, make that the head node now
+        """
+        # start off with None as the prev node
+        # get the current head node
+#         prev = None
+#         curr = head
+#         while curr:
+#             # here is where we do the setting of nodes
+#             next_node = curr.next
+#             curr.next = prev
+            
+#             # update the previous node to be the one we want the next node to attach to
+#             # update curr to go to the next_node
+#             prev = curr
+#             curr = next_node
+#         return prev # return the head node
+        return self.reverseListRecursive(head, None)
+    def reverseListRecursive(self, curr: Optional[ListNode], prev: Optional[ListNode]) -> Optional[ListNode]:
+        if not curr:
+            return prev
+        next_node = curr.next
+        curr.next = prev
+        return self.reverseListRecursive(next_node, curr)
+    
+        
+        
             
         
