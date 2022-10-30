d26e9c0cd1246e2784c01c4bb86de0c7f45e1279
Brendan Cheong <brendancej82@gmail.com>
2022-10-30 19:08:28 +0000

    [LeetCode Sync] Runtime - 53 ms (7.84%), Memory - 13.9 MB (100.00%)

--- 0226-invert-binary-tree/solution.py
+++ 0226-invert-binary-tree/solution.py
@@ -1,4 +1,6 @@
 # Definition for a binary tree node.
+from collections import deque
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -6,15 +8,39 @@ class TreeNode:
         self.right = right
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
+#         """
+#         Recursively, run through all the subtree and invert each subtree
+#         """
+#         if not root:
+#             return None
+#         # swap the left and right
+#         root.left, root.right = root.right, root.left
+#         # continue to recurse through the rest of the sub-trees
+#         self.invertTree(root.left)
+#         self.invertTree(root.right)
+#         return root
         """
-        Recursively, run through all the subtree and invert each subtree
+        Iteratively, this is a BFS algorithm using a queue. 
+        You can also do it using a DFS
         """
+        # 1. edge case
         if not root:
             return None
-        # swap the left and right
-        root.left, root.right = root.right, root.left
-        # continue to recurse through the rest of the sub-trees
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        q = []
+        q.append(root)
+        
+        while q:
+            curr = q.pop()
+            
+            # swap the nodes
+            curr.left, curr.right = curr.right, curr.left
+            
+            # add more nodes to the queue if there is
+            if (curr.left):
+                q.append(curr.left)
+                
+            if (curr.right):
+                q.append(curr.right)
         return root
+        
         
