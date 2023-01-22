88b778f0ed092d013b734e01bc14a7c4bfc1671b
Brendan Cheong <brendancej82@gmail.com>
2023-01-22 15:54:07 +0000

    [LeetCode Sync] Runtime - 28 ms (91.10%), Memory - 13.9 MB (100.00%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -3,7 +3,21 @@ class Solution:
         self.cache = {1: 1, 2: 2}
 
     def climbStairs(self, n: int) -> int:
+        """
+        O(n) time with caching, in this DP we start from the bottom aka the end result, and we have either go step down 1 or down 2 until we hit the result
+        """
         if n not in self.cache:
             self.cache[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
         return self.cache[n]
+    
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = one
+            one += two
+            two = temp
+        return one
+
+            
+        
         
