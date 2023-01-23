863a0852e10360b24976de2fdcfc2dac4f402761
Brendan Cheong <brendancej82@gmail.com>
2023-01-23 11:39:10 +0000

    [LeetCode Sync] Runtime - 89 ms (6.27%), Memory - 15.4 MB (100.00%)

--- 0206-reverse-linked-list/solution.py
+++ 0206-reverse-linked-list/solution.py
@@ -1,3 +1,5 @@
+from typing import *
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -5,32 +7,31 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        We want the current head to point to none
-        then the next node point to the previous node
-        do until the last node, make that the head node now
-        """
-        # start off with None as the prev node
-        # get the current head node
-#         prev = None
-#         curr = head
-#         while curr:
-#             # here is where we do the setting of nodes
-#             next_node = curr.next
-#             curr.next = prev
-            
-#             # update the previous node to be the one we want the next node to attach to
-#             # update curr to go to the next_node
-#             prev = curr
-#             curr = next_node
-#         return prev # return the head node
-        return self.reverseListRecursive(head, None)
-    def reverseListRecursive(self, curr: Optional[ListNode], prev: Optional[ListNode]) -> Optional[ListNode]:
-        if not curr:
+        def reverseListRecursive(curr: Optional[ListNode], prev: Optional[ListNode]) -> Optional:
+            if not curr:
+                return prev
+            next_node = curr.next
+            curr.next = prev
+            return reverseListRecursive(next_node, curr)
+        # return reverseListRecursive(head, None)
+
+        def reverseListIterative(head: Optional[ListNode]) -> Optional[ListNode]:
+            prev = None
+            curr = head
+
+            while curr:
+                next_node = curr.next
+                curr.next = prev
+
+                prev = curr
+                curr = next_node
             return prev
-        next_node = curr.next
-        curr.next = prev
-        return self.reverseListRecursive(next_node, curr)
+        return reverseListIterative(head)
+
+
+
+        
+
     
         
         
