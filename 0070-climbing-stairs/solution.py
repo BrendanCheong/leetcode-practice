7e284d6f3d06e896144afb0cc6e8dd098fe2abf1
Brendan Cheong <brendancej82@gmail.com>
2023-08-20 09:25:39 +0000

    [LeetCode Sync] Runtime - 34 ms (58.87%), Memory - 16.3 MB (98.92%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -4,19 +4,21 @@ class Solution:
 
     def climbStairs(self, n: int) -> int:
         """
-        O(n) time with caching, in this DP we start from the bottom aka the end result, and we have either go step down 1 or down 2 until we hit the result
+        O(n) time with caching, in this DP we start from the bottom aka the end result, and we have either go step down 1 or down 2 until we hit the result.
+
+        DP is just a recursion tree with caching
         """
         if n not in self.cache:
             self.cache[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        return self.cache[n]
+        return self.cache[n] # eventually this will be the root of the tree
     
-    def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n - 1):
-            temp = one
-            one += two
-            two = temp
-        return one
+    # def climbStairs(self, n: int) -> int:
+    #     one, two = 1, 1
+    #     for i in range(n - 1):
+    #         temp = one
+    #         one += two
+    #         two = temp
+    #     return one
 
             
         
