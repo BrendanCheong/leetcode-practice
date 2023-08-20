8b419fab9f5d4672dd8099329aef6d7b3a2a76e1
Brendan Cheong <brendancej82@gmail.com>
2023-08-20 08:49:21 +0000

    [LeetCode Sync] Runtime - 48 ms (82.53%), Memory - 16.5 MB (98.91%)

--- 0383-ransom-note/solution.py
+++ 0383-ransom-note/solution.py
@@ -1,7 +1,9 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        words = set(ransomNote) 
-        for i in words: 
-            if magazine.count(i) < ransomNote.count(i): 
-                return False 
-        return True 
+        counter1 = Counter(ransomNote)
+        counter2 = Counter(magazine)
+        if counter1 & counter2 == counter1:
+            return True
+        return False
