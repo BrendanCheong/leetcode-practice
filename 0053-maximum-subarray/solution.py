598d09eb6c1830456532f31a582ca06c32d91038
Brendan Cheong <brendancej82@gmail.com>
2023-08-22 13:05:18 +0000

    [LeetCode Sync] Runtime - 1376 ms (5.05%), Memory - 352.7 MB (5.03%)

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -1,12 +1,32 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_num = nums[0]
-        curr = 0 # initial value
+        """
+        DP memoised recursion tree solution
+        """
+        from functools import cache
+        @cache
+        def solve(i, must_pick):
+            # Base Case, if reach end of array, I must decide to end
+            # if I must pick the next num but end of array, pick 0
+            # if no need to pick the next num, just return the smallest possible num 
+            # so it doesn't mess with the max() function 
+            if i >= len(nums):
+                if must_pick:
+                    return 0
+                else:
+                    return float("-inf")
+
+            # Include current number and go on to the next number
+            # this is the first decision
+            include_current = nums[i] + solve(i + 1, True)
+
+            # Exclude current number (only if must_pick is False)
+            # this is the second decision
+            exclude_current = 0 if must_pick else solve(i + 1, False)
+
+            return max(include_current, exclude_current)
+
+        return solve(0, False)
         
-        for ele in nums:
-            if curr < 0: # if adding a ele gets a negative value, reset to 0 to get max sum
-                curr = 0
-            curr += ele
-            max_num = max(curr, max_num)
-        return max_num
+
         
