227036901d975af6aea8d9225564e49bb05b12d3
Brendan Cheong <brendancej82@gmail.com>
2024-04-29 14:59:29 +0000

    [LeetCode Sync] Runtime - 86 ms (73.56%), Memory - 22.1 MB (14.15%)

--- src/0347-top-k-frequent-elements/solution.py
+++ src/0347-top-k-frequent-elements/solution.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from typing import *
 import heapq
 
 class Solution:
@@ -8,30 +7,57 @@ class Solution:
         You can use the bucket sort algo to solve this qns in O(n) time.
         Modified bucket sort, instead of index = element and value = occurence,
         we do index = occurence, value = element
+        Time complexity is O(n)
+        Space complexity is O(n)
+        """
+
+        # default value is 0
+        hashmap = defaultdict(int)
+        for num in nums:
+            hashmap[num] += 1
+        
+        # The max array size is k + 1, as we cannot have 0 array size since there
+        # will always be an occurence, so we do + 1
+        # Ex: if the array is size 6, could we have 100 max occurences of lets say "1"?
+        # of course not, then we would need an array size >= 100 
+        buckets = [[] for n in range(len(nums) + 1)]
+
+        # populate our bucket, we can have multiple numbers having the same occurence
+        # like number 10, can occur once, so can number 1
+        # our bucket looks like this -> [[], [10, 1], [2]]
+        for num, freq in hashmap.items():
+            buckets[freq].append(num)
+
+        res = []
+        # iterate backwards, its faster this way, cuz we want the highest occuring number (k)
+        # not the lowest occuring number
+        for i in range(len(buckets) - 1, 0, -1):
+            bucket = buckets[i]
+            for n in bucket:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+    def topKFrequentHeap(self, nums: List[int], k: int) -> List[int]:
+        """
+        You can also use a heap to solve this qns.
+        Time complexity is O(nlogk)
+        Space complexity is O(n)
         """
         # populate the dictionary with all occurences
         hashmap = defaultdict(int)
         for i in nums:
             hashmap[i] += 1
 
-        
-        # create a frequency bucket array. Each index of this array is the possible highest occurence
-        # of the nums array. Ie: the max occurence = max_occur + 1
-        # why use max + 1 and not just max? cos we don't have 0 occurences, so the index is usually 0, 1, 2...
-        # we want to + 1 so we get to the end
-
-        freq = [[] for n in range(len(nums) + 1)]
-
-        # populate the freq array
+        # create a heap
+        heap = []
         for n, v in hashmap.items():
-            freq[v].append(n)
+            heapq.heappush(heap, (v, n))
+            # If the heap is larger than k, we pop the smallest element 
+            # because we want to keep the largest k elements
+            if len(heap) > k:
+                heapq.heappop(heap)
 
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-            
-        
-        
+        return [n for v, n in heap]
+
+        return [n for v, n in heap]
