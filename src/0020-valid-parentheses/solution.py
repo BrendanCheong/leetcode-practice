648effa64bd0fef4c2e702f073f8eed79bcc82b4
Brendan Cheong <brendancej82@gmail.com>
2024-05-01 08:49:55 +0000

    [LeetCode Sync] Runtime - 29 ms (90.49%), Memory - 16.5 MB (74.60%)

--- src/0020-valid-parentheses/solution.py
+++ src/0020-valid-parentheses/solution.py
@@ -1,23 +1,27 @@
-from typing import *
-
 class Solution:
     def isValid(self, s: str) -> bool:
         """
-        Edge cases: What if the string is equal to one?
-        what if the string is (()), its still invalid!
+        Algo is simple O(n) time, O(n) space.
+        We keep adding starting brackets until we can find a closing bracket.
+        By popping, we make sure we are always finding the best closing bracket
         """
-        Map = {")": "(", "]": "[", "}": "{"}
+        guide = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
         stack = []
-
-        for c in s:
-            if c not in Map:
-                stack.append(c)
+        for char in s:
+            if char not in guide:
+                stack.append(char)
                 continue
-            if not stack or stack[-1] != Map[c]:
-                return False
-            stack.pop()
-
+            else:
+                if not stack or guide[char] != stack[-1]:
+                    return False
+                # Here we find that current {char} matches the top of stack
+                # so we can close the bracket. Once closed, remove from stack
+                # to move on to next character
+                stack.pop()
+        # Stack must be emptied
         return not stack
-                
-            
-            
+        
