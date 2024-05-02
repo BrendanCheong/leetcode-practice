2da9e1c8929b7fcd56084b505bb2e8fb254cbcd1
Brendan Cheong <brendancej82@gmail.com>
2024-05-02 15:57:17 +0000

    [LeetCode Sync] Runtime - 60 ms (30.10%), Memory - 20.5 MB (34.92%)

--- src/0155-min-stack/solution.py
+++ src/0155-min-stack/solution.py
@@ -1,33 +1,34 @@
-TOP = -1
-
 class MinStack:
 
     def __init__(self):
-        self.norm_stack = []
-        self.min_stack = []
+        self.current_min = []
+        self.stack = []
 
     def push(self, val: int) -> None:
-        self.norm_stack.append(val)
-        if (not len(self.min_stack)):
-            self.min_stack.append(val)
-            return
-
-        min_stack_top = self.min_stack[TOP]
-        if (val <= min_stack_top):
-            self.min_stack.append(val)
+        self.stack.append(val)
+        # update the current min number
+        if not self.current_min:
+            self.current_min.append(val)
+        else:
+            new_num = min(val, self.current_min[-1])
+            # we will add to the stack anyways if the val is bigger than the 
+            # current minimum. This means that for this particular value
+            # when we are capturing the state at which the stack currently is
+            # so when we pop(), its ok to remove from the min stack
+            # so if the val = 5 and current min is -1
+            # its still [-1, -1] vs [0, 5], which is ok
+            self.current_min.append(new_num)
 
     def pop(self) -> None:
-        if (self.min_stack[TOP] == self.norm_stack[TOP]):
-            self.min_stack.pop()
-        
-        self.norm_stack.pop()
+        self.stack.pop()
+        # update the current_min
+        self.current_min.pop()
 
     def top(self) -> int:
-        return self.norm_stack[TOP]
-        
+        return self.stack[-1]
+
     def getMin(self) -> int:
-        return self.min_stack[TOP]
-        
+        return self.current_min[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
