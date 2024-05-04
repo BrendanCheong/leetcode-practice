034ea19520278102b607683d0a5aad8372d04f9f
Brendan Cheong <brendancej82@gmail.com>
2024-05-04 11:24:38 +0000

    [LeetCode Sync] Runtime - 42 ms (73.03%), Memory - 17 MB (63.34%)

--- src/0125-valid-palindrome/solution.py
+++ src/0125-valid-palindrome/solution.py
@@ -1,25 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # do the 2 pointers technique
-        # this is a O(1) space technique
-        # With a O(n) time
-        start = 0
-        end = len(s) - 1
-        while start < end: # iterate until start passes the end
-            while not s[start].isalnum() and start < end:
-                # skip spaces/non-alphanumeric characters
-                # do not execute if the pointers pass each other (edge case)
-                start += 1 # move pointer towards end
-            while not s[end].isalnum() and start < end:
-                # same idea here
-                end -= 1 # move pointer to the start
-            # if statement must come first to check edge case of beginning letter
-            # account for edge case of lowercased letters.
-            # do not do s = s.lower() as its O(N) time
-            if (s[start].lower() != s[end].lower()):
+        """
+        2 pointer solution with minimal memory usage.
+        Have 2 pointers, one on each end of the string
+        left pointer goes right, right pointer goes left, stop until they cross
+        Skip non alpha numeric characters by just progressing the pointers when you see one
+        For each character we compare it, if its not the same we return False
+
+        Time: O(n)
+        Space: O(1)
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            if s[right].lower() != s[left].lower():
                 return False
-            start += 1
-            end -= 1
+            left += 1
+            right -= 1
         return True
-        
-        
+
