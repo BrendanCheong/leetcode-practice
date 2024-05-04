a3bc76eccb86570b2e1cf2c1f7b0ee271fea8b25
Brendan Cheong <brendancej82@gmail.com>
2024-05-04 11:44:33 +0000

    [LeetCode Sync] Runtime - 43 ms (99.39%), Memory - 17.9 MB (9.43%)

--- src/0001-two-sum/solution.py
+++ src/0001-two-sum/solution.py
@@ -1,24 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        Time Complexity: O(n), with O(1 + α) amortized HashTable operation
-        Explanation: Each element has its leftover amount given the target
-        like for example, given target 9, and element 2 will have leftover 7
-        and element 7 will have leftover 2.
-        
-        When there is a pair of leftovers that are inside the array, we have an answer.
-        We can store each {ele: leftover} and check to see if the leftover is in the HashTable
-        to create this pair, for each element.
-        
-        If not in there, we can form the pair in the HashTable using {leftover: index}
-        We use a HashTable for fast querying of O(1) time, we take O(n) time to loop through 
-        the whole HashTable
+        This uses the Hashtable method, we basically keep track of every number's desired number
+        So if target is t = 9, and current number is 2, the desired is 7
+        keep this in a hashmap, like so Map[desired number] = current number's index
+        Then for each number we check if the desired number is in there
+        Return if it is. Note that there can be negative numbers in the array
+
+        Time: O(n)
+        Space: O(n)
         """
-        dct = dict()
-        for i, ele in enumerate(nums):
-            leftover = target - ele
-            if (leftover in dct): # if leftover is present
-                return [dct[leftover], i]
-            dct[ele] = i # add the index to the hashmap
-        return [0, 0]
-        
+        mem = dict()
+        for index, num in enumerate(nums):
+            if num in mem:
+                first, second = mem[num], index
+                return [first, second]
+            mem[target - num] = index
