e253e4f6af75708ae11544b408831d4ed80dd623
Brendan Cheong <brendancej82@gmail.com>
2024-05-09 09:52:15 +0000

    [LeetCode Sync] Runtime - 92 ms (47.68%), Memory - 19.7 MB (68.47%)

--- src/0049-group-anagrams/solution.py
+++ src/0049-group-anagrams/solution.py
@@ -1,32 +1,27 @@
-from collections import defaultdict
-import string
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         """
-        Naive solution would be to create a dictionary for each word
-        then O(n^2) loop, for each word find the matching dictionary with
-        the same value
-       
-        Another idea is to sort the words, but its gonna take N log N for M times which is M = length of array
-        
-        The trick is that in order to find matching words in O(1) time, we have to use tuples!!! and HashMaps. So each word has its own tuple (which is the character count) and we can match the word(s) to its tuple(s)
-        
-        This is O(m * n) time, m number of words, n length of each word
-        space is O(n)
+        If we sort a word that is an anagram, we essentially get the common anagram word
+        Like if we sort "eat" and "tea" we get both "aet"
+        We can then use hashmaps to store these results
+
+        You could also speed up the default sort using bucket sort, which is O(n) sort
+        and not n log n
+
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+        ans[tuple(count)].append(s)
+
+        Since max length is n = 100, fastest speed is O(100), slow sort is O(100 log(100))
+
+        Time: O(n * n log n) if not bucket sort
+        O(n) if bucket sort
+        Space: O(n)
         """
-        res = defaultdict(list) # dct of character count arrays
-        
+        hashmap = defaultdict(list)
         for i, word in enumerate(strs):
-            char_arr = [0] * 26 # create blank char arr for each word
-            for j in word:
-                char_arr[ord(j) - ord('a')] += 1
-            # now that we have our char arr we can add it to our dict
-            # have to use tuple to make it unique
-            # add the corresponding word to char_arr
-            res[tuple(char_arr)].append(word)
-        # only return the values with the array
-        return res.values()
-            
-                
-        
+            common_word = ''.join(sorted(word))
+            hashmap[common_word].append(word)
+
+        return list(hashmap.values())
