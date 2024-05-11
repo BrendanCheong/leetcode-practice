ff1c140609884b1794a3d926d944cad09d180d51
Brendan Cheong <brendancej82@gmail.com>
2024-05-11 02:13:36 +0000

    [LeetCode Sync] Runtime - 193 ms (47.76%), Memory - 17.9 MB (99.45%)

--- src/0792-binary-search/solution.py
+++ src/0792-binary-search/solution.py
@@ -1,34 +1,33 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # left_index: int = 0
-        # right_index: int = len(nums) - 1
-        
-        # while left_index <= right_index:
-        #     # find the middle of the array
-        #     # stop when the left pointer is more than the right, in which we have exceeded the target and cannot find anything 
-        #     mid_index: int = left_index + (right_index - left_index) // 2
-            
-        #     if nums[mid_index] == target:
-        #         return mid_index
-        #     elif nums[mid_index] < target:
-        #         # if I am less than the target, it means that I must go right
-        #         # so bring left pointer to the right side more
-        #         left_index = mid_index + 1
-        #     elif nums[mid_index] > target:
-        #         # I must go left as I am too big, so right goes smaller
-        #         right_index = mid_index - 1
-        # # if left pointer exceeds then we cannot find the index
-        # # in which element is not present in the array
-        # return -1
+        """
+        Since the array is already sorted, we can use binary search.
+        Note that middle of array is first pointer + (last - first) // 2
+
+        Also note that binary search is about chopping the array into half based
+        on the 2 pointers
+
+        You may find that other binary search qns have different if statements
+        But all evolve around left/right = middle -1/+1
+
+        Time: O(log n)
+        Space: O(1)
+        """
         left, right = 0, len(nums) - 1
 
+        # We use left <= right and not left < right
+        # for edge case when array is length 1, like arr = [5]
         while left <= right:
-            mid = (right + left) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
+            middle = left + ((right - left) // 2)
+            if nums[middle] < target:
+                # we are too small, so we need to go bigger
+                # which is the right hand side
+                left = middle + 1
+            elif nums[middle] > target:
+                # we are too big, so we need go smaller
+                # which is left hand side
+                right = middle - 1
             else:
-                right = mid - 1
+                return middle
+        # If no answer can be found
         return -1
-        
