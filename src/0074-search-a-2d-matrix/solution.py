24f1cb70d9ad5cd6d7724432f2887231c2a4ad7a
Brendan Cheong <brendancej82@gmail.com>
2024-05-11 03:03:35 +0000

    [LeetCode Sync] Runtime - 48 ms (40.08%), Memory - 17 MB (98.17%)

--- src/0074-search-a-2d-matrix/solution.py
+++ src/0074-search-a-2d-matrix/solution.py
@@ -1,35 +1,52 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROW, COL = len(matrix), len(matrix[0])
-        top, bottom = 0, ROW - 1
+        """
+        This qns is quite interesting in the sense that its just 2 binary searches
+        The trick is determining the first binary search, which row is the target in?
+        We can alter our if statements for the first binary search to check if the
+        target is within the row itself by looking at the range of values, aka the 
+        start number of the array and end number of the array, where the target must be in between that
+
+        After finding the row we can just binary search on that row
+
+        Note: Must account for when target is the exact value of the head or end of the row, we need
+        <= and >= operators
+
+        Time: O(log (m * n)), m = row, n = col
+        Space: O(1)
+        """
+        # Finding matrix values is always Matrix[ROW][COL]
+        ROWS, COLS = len(matrix), len(matrix[0])
+        selected_row = 0
+
+        # Lets determine which row the answer is in first!
+        top, bottom = 0, ROWS - 1
         while top <= bottom:
-            row = top + (bottom - top) // 2
-            first, last = matrix[row][0], matrix[row][-1]
-            if first > target:
-                bottom = row - 1
-            elif first < target and last < target:
-                top = row + 1
-            else:
+            mid_row = top + ((bottom - top) // 2)
+
+            # Need to account for if the target is exactly at the head or end
+            # of the row, so we need <= and >=
+            if matrix[mid_row][0] <= target and matrix[mid_row][-1] >= target:
+                selected_row = mid_row
                 break
-        target_index = (top + bottom) // 2
-
-        # check out of bounds edge cases
-        if target_index > ROW or target_index < 0:
-            return False
-
-        # conduct binary search on target row as per usual
-        target_col = matrix[target_index]
-
-        def binary_search(arr: List[int], target: int) -> bool:
-            left, right = 0, len(arr) - 1
-            while left <= right:
-                mid = left + (right - left) // 2
-                if arr[mid] > target:
-                    right = mid - 1
-                elif arr[mid] < target:
-                    left = mid + 1
-                else:
-                    return True
-            return False
-        return binary_search(target_col, target)
+            elif matrix[mid_row][0] < target and matrix[mid_row][-1] < target:
+                top = mid_row + 1
+            elif matrix[mid_row][0] > target and matrix[mid_row][-1] > target:
+                bottom = mid_row - 1
+        
+        # Now that we have found our selected row, we can do regular binary search
+        arr = matrix[selected_row]
+        return self.binarySearch(arr, target)
+    
+    def binarySearch(self, arr: List[int], target: int) -> bool:
+        left, right = 0, len(arr) - 1
+        while left <= right:
+            middle = left + ((right - left) // 2)
+            if arr[middle] < target:
+                left = middle + 1
+            elif arr[middle] > target:
+                right = middle - 1
+            else:
+                return True
+        return False
 
