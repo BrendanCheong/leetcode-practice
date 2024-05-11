087bce5ffa37052463e68945f7dcb6a7e5272b39
Brendan Cheong <brendancej82@gmail.com>
2024-05-11 04:47:48 +0000

    [LeetCode Sync] Runtime - 40 ms (55.80%), Memory - 16.6 MB (41.14%)

--- src/0069-sqrtx/solution.py
+++ src/0069-sqrtx/solution.py
@@ -1,29 +1,21 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         """
-        This question is secretly a binary search question
-        
-        Think of Brute force first, we would check every number from 1
-        x 1 all the way to n x n to find the closet number. Example:
-        
-        1 x 1 = 1
-        2 x 2 = 4
-        3 x 3 = 9
-        
-        we can use binary search to find the correct digit. Upper bound is always  x // 2 since square root of the number is always less . Edge case for when if x < 2, the only square root is itself
+        This is a binary search question, as we can go from 0 to the ideal number by seeing if the product is
+        close to the answer of x
+
+        So we test by having mid * mid, where mid is between 0 and x
+
+        Time: O(log n)
+        Space: O(1)
         """
-        if x >= 2:
-            left, right = 2, x
-            
-            while left <= right:
-                mid = left + (right - left) // 2
-                number = mid * mid
-                if number > x:
-                    right = mid - 1
-                elif number < x:
-                    left = mid + 1
-                else:
-                    return mid
-            return right
-        return x
-        
+        left, right = 0, x
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                return mid
+        return right
