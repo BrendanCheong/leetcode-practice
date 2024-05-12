84c63f492c2acb5eb4e0039a0add04eb80e85120
Brendan Cheong <brendancej82@gmail.com>
2024-05-12 05:51:45 +0000

    [LeetCode Sync] Runtime - 413 ms (73.21%), Memory - 32 MB (65.45%)

--- src/0217-contains-duplicate/solution.py
+++ src/0217-contains-duplicate/solution.py
@@ -1,10 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         """
-        O(n) solution, we just add to a set and see if the element
-        is already inside
+        The most obvious solution is a hashset to keep track of all the numbers you have seen
+        If I have seen a number in my hashset/memory, then I have found the answer.
+
+        A low Big-O space would be to sort and use bit manipulation, but thats
+        O(n log n) time and O(1) space
+
+        Time: O(n)
+        Space: O(n)
         """
-        if len(nums) == len(set(nums)):
-            return False
-        return True
-        
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
