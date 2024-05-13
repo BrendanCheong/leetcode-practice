5f17d52034f611d1b1fb765d85937b9686f84010
Brendan Cheong <brendancej82@gmail.com>
2024-05-13 07:20:46 +0000

    [LeetCode Sync] Runtime - 502 ms (21.54%), Memory - 29 MB (90.98%)

--- src/0215-kth-largest-element-in-an-array/solution.py
+++ src/0215-kth-largest-element-in-an-array/solution.py
@@ -1,23 +1,19 @@
+from heapq import heapify, heappop
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k  = len(nums) - k # to find k largest, for k smallest its k - 1
-        def quickSelect(l: int, r: int) -> int:
-            pivot = nums[r]
-            p = l
-            for  i in range(l, r):
-                if (nums[i] <= pivot): # iterate through whole array
-                    nums[p], nums[i] = nums[i], nums[p] # swap the partitioned value IF "r" is the biggest, to move on to next biggest
-                    p += 1 # increment the p pointer so we get the final place where we can partition
-            nums[p], nums[r] = nums[r] , nums[p] # last swap to get new pivot
-            if (p > k):
-                return quickSelect(l, p - 1) # look at the left portion of the array
-            elif (p < k):
-                return quickSelect(p + 1, r); # look at the right portion of the array
-            else:
-                return nums[p]
-        return quickSelect(0, len(nums) - 1) # pivot selected is just the last value
-            
-                    
-                    
-                    
+        """
+        You can use min heap for this solution, but its not the most
+        memory efficient. A better method would be to use partition and quick select,
+        components of quicksort!
+
+        Partition and Heaps go hand in hand for kth elements!
+
+        Time: O(n k log n)
+        Space: O(n)
+        """
+        heapify(nums)
+        while len(nums) > k:
+            heappop(nums)
+        return nums[0]
         
