d9739057818d36fe24ad8aaae62ecd5d9338a6ec
Brendan Cheong <brendancej82@gmail.com>
2024-05-13 07:10:56 +0000

    [LeetCode Sync] Runtime - 23 ms (99.31%), Memory - 16.5 MB (41.80%)

--- src/1127-last-stone-weight/solution.py
+++ src/1127-last-stone-weight/solution.py
@@ -1,15 +1,27 @@
-import heapq
+from heapq import heapify, heappush, heappop
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones = [-s for s in stones]
-        heapq.heapify(stones)
+        """
+        A typical max heap qns, anything with kth largest elements involves heaps
 
-        while len(stones) > 1:
-            first = heapq.heappop(stones)
-            second = heapq.heappop(stones)
-            if second > first:
-                heapq.heappush(stones, first - second)
+        We'll just add negative numbers when taking out of the heap and adding into the heap
+        if we want to create max heaps
 
-        stones.append(0)
-        return abs(stones[0])
+        Time: O(n * nlogk)
+        Space: O(n)
+        """
+        max_heap = [-stone for stone in stones]
+        heapify(max_heap)
+
+        while len(max_heap) > 1:
+            heaviest = heappop(max_heap)
+            second_heaviest = heappop(max_heap)
+            if heaviest != second_heaviest:
+                new_stone = (-heaviest) - (-second_heaviest)
+                heappush(max_heap, -new_stone)
+
+        # edge case when both stones at the begining gets destroyed, we 
+        # have to return something
+        return -max_heap[0] if max_heap else 0
+        
