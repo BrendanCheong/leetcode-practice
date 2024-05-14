1fe6dec9ff09bce1ec19d37a01206380a26bb0f8
Brendan Cheong <brendancej82@gmail.com>
2024-05-14 12:26:02 +0000

    [LeetCode Sync] Runtime - 50 ms (11.01%), Memory - 16.8 MB (72.36%)

--- src/0153-find-minimum-in-rotated-sorted-array/solution.py
+++ src/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -1,25 +1,42 @@
-from typing import *
-
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
+        """
+        In this case, whenever we rotate, we will always put larger values to
+        the left of the array (thats what the qns describes). Since there is always
+        at least one rotation, we can conclude that one part of the array contains sorted
+        bigger numbers, the other sorted smaller numbers.
 
-        if nums[left] < nums[-1]:
-            return nums[left]
+        If we're in the so called smaller numbers, we probably already found our answer,
+        but we should check the other sorted portion of the array just in case
+        Elsewise, if we're in the big sorted, check the smaller sorted numbers.
 
-        if len(nums) == 1:
-            return nums[0]
-        
+        The array is not rotated at all, meaning the smallest element is at the beginning.
+
+        The array is rotated, which creates a pivot point where the pattern changes.
+
+        nums[mid] >= nums[left]
+            - search right
+        nums[mid] < nums[right]
+            - search left
+
+        Time: O(log n)
+        Space: O(1)
+        """
+        left, right, current_min = 0, len(nums) - 1, float("inf")
         while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] > nums[0]:
-                left = mid + 1
+            middle = left + ((right - left) // 2)
+            current_min = min(current_min, nums[middle])
+
+            # The left has the minimum
+            # Array is likely not rotated, so smallest starts from beginning
+            if nums[right] > nums[middle]:
+                right = middle - 1
+
+            # The right has the minimum
+            # Array is likely rotated, so smallest starts at the right far end
             else:
-                right = mid - 1
-            
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            elif nums[mid] < nums[mid - 1]:
-                return nums[mid]
-        return -1
+                left = middle + 1
+
+        return current_min
+
 
