7690e83c53790b71cb6e30b2a2c9524a8668b6e4
Brendan Cheong <brendancej82@gmail.com>
2024-05-16 12:03:59 +0000

    [LeetCode Sync] Runtime - 684 ms (92.01%), Memory - 27.4 MB (34.01%)

--- src/0121-best-time-to-buy-and-sell-stock/solution.py
+++ src/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,39 +1,25 @@
-# link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
-from typing import List
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
-        Solution is an O(N) solution with one iteration, followed by O(1) space.
-        You could probably use dequeues to keep track of the sliding window, but that would be O(N) space.
+        Greedy sliding window problem, we compute every scenario where we can get 
+        a profit!
+
+        The sliding window starts at the smallest price we found so far
+        Then compute the profit when the current price goes high (but low sell high)
+
+        Time: O(n)
+        Space: O(1)
         """
-        # edge case
-        if len(prices) < 2:
-            return 0
-        left, right = 0, 1
-        MAX = -1
-
-        # terminate when right is out-of-bounds of the array
-        while right < len(prices):
-            # need to immediately calculate since we start of from index 0 and 1
-            if prices[left] < prices[right]:
-                MAX = max(MAX, prices[right] - prices[left])
-                right += 1
-                # don't move the left since we found the best spot to buy at the moment
-            elif prices[left] >= prices[right]:
-                # if we find that we cannot make a profit, move the left pointer forward
-                # then we cannot have both pointers at the same place, so move the right forward
-                left = right
-                right += 1
-            
-        return 0 if MAX == -1 else MAX
+        min_price, profit = prices[0], 0
+        for p in prices:
+            if p < min_price:
+                # update the new minimum price, this is the start of
+                # our sliding window
+                min_price = p
+            # Greedily compute every scenario where the current price
+            # can bring us a profit
+            elif p - min_price > profit:
+                profit = max(p - min_price, profit)
+        return profit
 
 
-sol = Solution()
-print(sol.maxProfit([7,1,5,3,6,4]))
-assert sol.maxProfit([7,1,5,3,6,4]) == 5
-print(sol.maxProfit([7,6,4,3,1]))
-assert sol.maxProfit([7,6,4,3,1]) == 0
-print(sol.maxProfit([1,2,4,2,5,7,2,4,9,0,9]))
-assert(sol.maxProfit([1,2,4,2,5,7,2,4,9,0,9])) == 9
