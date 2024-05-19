5748f7308d8d58b2a2e277a80a87f8aacead698c
Brendan Cheong <brendancej82@gmail.com>
2024-05-19 01:22:22 +0000

    [LeetCode Sync] Runtime - 45 ms (9.18%), Memory - 17.6 MB (97.69%)

--- src/0206-reverse-linked-list/solution.py
+++ src/0206-reverse-linked-list/solution.py
@@ -1,42 +1,26 @@
-from typing import *
-
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val: int = 0, next: int = None):
         self.val = val
-        self.next = next
+        self.next = next # By default the tail of the LL is None
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def reverseListRecursive(curr: Optional[ListNode], prev: Optional[ListNode]) -> Optional:
-            if not curr:
-                return prev
-            next_node = curr.next
+        """
+        You start with None, which is the actual head of the linked list
+        Then have 2 pointers to keep track of nodes so we can reverse it
+        by relinking it backwards.
+        
+        Time: O(n)
+        Space: O(1)
+        """
+        prev, curr = None, head
+        while curr:
+            temp = curr.next
             curr.next = prev
-            return reverseListRecursive(next_node, curr)
-        # return reverseListRecursive(head, None)
-
-        def reverseListIterative(head: Optional[ListNode]) -> Optional[ListNode]:
-        # start off with none and head node
-            prev, curr = None, head
-            while curr:
-                next_node = curr.next
-                curr.next = prev
-                
-                # move the prev first, since we need to make the curr point to
-                # the prev
-                prev = curr
-                curr = next_node
-            return prev
-    
             
-        return reverseListIterative(head)
-
-
-
-        
-
-    
-        
-        
-            
-        
+            # Update the previous to go forward
+            prev = curr
+            # Update the current to go forward
+            curr = temp
+        return prev # the new head of the Linked List
