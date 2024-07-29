787ea8fc10e0bd328b21353441643ea206d7ccb7
Brendan Cheong <brendancej82@gmail.com>
2024-07-29 08:30:26 +0000

    [LeetCode Sync] Runtime - 39 ms (29.85%), Memory - 16.4 MB (91.86%)

--- src/0226-invert-binary-tree/solution.py
+++ src/0226-invert-binary-tree/solution.py
@@ -1,46 +1,17 @@
 # Definition for a binary tree node.
-from collections import deque
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
-#         """
-#         Recursively, run through all the subtree and invert each subtree
-#         """
-#         if not root:
-#             return None
-#         # swap the left and right
-#         root.left, root.right = root.right, root.left
-#         # continue to recurse through the rest of the sub-trees
-#         self.invertTree(root.left)
-#         self.invertTree(root.right)
-#         return root
-        """
-        Iteratively, this is a BFS algorithm using a queue. 
-        You can also do it using a DFS
-        """
-        # 1. edge case
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return None
-        q = []
-        q.append(root)
-        
-        while q:
-            curr = q.pop()
-            
-            # swap the nodes
-            curr.left, curr.right = curr.right, curr.left
-            
-            # add more nodes to the queue if there is
-            if (curr.left):
-                q.append(curr.left)
-                
-            if (curr.right):
-                q.append(curr.right)
+            return root
+
+        # swap the left and right, then continue down the tree
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
         return root
-        
-        
