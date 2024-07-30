efdefdffee52caab4be899606308b153685151da
Brendan Cheong <brendancej82@gmail.com>
2024-07-30 03:34:02 +0000

    [LeetCode Sync] Runtime - 33 ms (71.78%), Memory - 16.5 MB (14.36%)

--- src/0094-binary-tree-inorder-traversal/solution.py
+++ src/0094-binary-tree-inorder-traversal/solution.py
@@ -1,26 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         """
-        left -> root -> right
-        but with stack its in reverse order
+        In order traversal is like going through a sorted array
+        from LEFT to RIGHT
+        so its left, root, right
+        thats why its called inorder, cuz the output is the sorted BST
         """
-        visited, stack = set(), []
         res = []
-        stack.append(root)
-
-        while stack:
-            node = stack.pop()
-            if node and node in visited:
+        def dfs(node: Optional[TreeNode]) -> None:
+            if node:
+                dfs(node.left)
                 res.append(node.val)
-            elif node:
-                stack.append(node.right)
-                stack.append(node)
-                visited.add(node)
-                stack.append(node.left)
+                dfs(node.right)
+        dfs(root)
         return res
