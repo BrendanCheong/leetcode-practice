e3bf9acfcc7b961238bc043bde91209a1fdde511
Brendan Cheong <brendancej82@gmail.com>
2024-07-30 04:38:27 +0000

    [LeetCode Sync] Runtime - 45 ms (22.21%), Memory - 17.2 MB (27.85%)

--- src/0102-binary-tree-level-order-traversal/solution.py
+++ src/0102-binary-tree-level-order-traversal/solution.py
@@ -1,27 +1,32 @@
 from collections import deque
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        levels = []
-        if not root:
-            return levels
-        height = 0
-        q = deque()
-        q.append(root)
-        while q:
-            levels.append([])
-            for i in range(len(q)):
-                node = q.popleft()
-                levels[height].append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            height += 1
-        return levels
+        queue = deque()
+        res = []
 
+        if root:
+            queue.append(root)
+        level = 1
+
+        while queue:
+            print(f"Current level: {level}")
+            level_arr = []
+            # empty the queue
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                level_arr.append(curr.val)
+                # add to queue
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            level += 1
+            res.append(level_arr)
+        return res
