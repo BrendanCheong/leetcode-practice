b08e968263ee48bb56ea98b061e9c4eadbe721d1
Brendan Cheong <brendancej82@gmail.com>
2024-07-30 09:47:44 +0000

    [LeetCode Sync] Runtime - 45 ms (88.14%), Memory - 16.6 MB (32.49%)

--- src/0039-combination-sum/solution.py
+++ src/0039-combination-sum/solution.py
@@ -1,22 +1,31 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        subset = []
-        def dfs(i: int, total: int):
+
+        def dfs(i: int, curr: List[int], total: int):
+            # when we reached the end of the tree, the leaf nodes
+            # we stop when we reached the target
             if total == target:
-                res.append(subset.copy())
+                # we will reuse this later, so create a copy of it
+                # so we don't override it
+                res.append(curr.copy())
                 return
+            
+            # we can't find any more combo when we can no longer add anymore items to the combo
+            # or when our total exceeds the target
             if i >= len(candidates) or total > target:
                 return
-            
-            subset.append(candidates[i])
 
-            # decision 1: add the same number
-            dfs(i, total + candidates[i])
-            
-            # decision 2: add the next number
-            subset.pop()
-            dfs(i + 1, total)
-        dfs(0, 0)
+            # left subtree, add to the combo and stay at that candidate
+            # add to the total as well
+            curr.append(candidates[i])
+            dfs(i, curr, total + candidates[i])
+
+            # right subtree, don't add to the combo at all, stay as it is
+            # so we pop from the combo so the combo stays as it is
+            # and we move on to the next candidate
+            curr.pop()
+            dfs(i + 1, curr, total)
+
+        dfs(0, [], 0)
         return res
-
