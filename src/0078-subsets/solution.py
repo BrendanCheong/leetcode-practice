d3a29edd55a8119d39039e91ccdf84f9f169c9df
Brendan Cheong <brendancej82@gmail.com>
2024-07-30 09:22:46 +0000

    [LeetCode Sync] Runtime - 27 ms (97.79%), Memory - 16.9 MB (20.30%)

--- src/0078-subsets/solution.py
+++ src/0078-subsets/solution.py
@@ -1,31 +1,42 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         """
-        WE create a backtracking graph
+        This requires a Decision-tree, where we can form all the decisions
+        then iterate through every decision to create the subsets needed
 
-        The left part of the subtree includes the number in the array
-        the right part of the subtree does not
-        recursively form this tree
-                /     \
-              [1]     []
-        add 2 / \     / \
-         [1, 2]  [1] [2] []
+        The decisions are:
+        1. Include the number in the array
+        2. Don't include the number in the array
+        We go down this decision tree until we can add no more numbers to the subset
+
+                / \ 
+            [1]    []
+            / \   /  \
+        [1,2] [1][2]  []
+        /    \
+     [1,2,3] [1,2]
+        and so on...
+        We stop when we run out of numbers to add to the subset
+        That means we are at the leaf nodes of the decision tree
+
+        Time: O(2 ^ n), since we are going to build every decision possible
         """
         res = []
         subset = []
-        def dfs(index: int) -> None:
-            # base condition, if index increases out of bounds, we have found the answer
-            if index >= len(nums):
-                res.append(subset[:]) # add a copy of the subset
+        def dfs(i: int) -> None:
+            # base case, we cannot add anymore numbers to the subset
+            if i >= len(nums):
+                res.append(subset.copy())
                 return
-            
-            # left subtree adds elements from the array
-            subset.append(nums[index])
-            dfs(index + 1)
 
-            # right subtree does not add elements, since we added before we just pop it out
+            # The left side of the tree adds numbers to the subset
+            subset.append(nums[i])
+            dfs(i + 1) # go through left subtree
+
+            # The right side of the tree doesn't add numbers to the subset
+            # in that case, we pop from the subset
             subset.pop()
-            dfs(index + 1)
+            dfs(i + 1) # go through right subtree
+         
         dfs(0)
         return res
-
