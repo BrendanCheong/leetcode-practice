29c5cf65066f4ce61cea2eaf4f04942340863227
Brendan Cheong <brendancej82@gmail.com>
2024-08-06 09:06:46 +0000

    [LeetCode Sync] Runtime - 40 ms (25.03%), Memory - 16.8 MB (8.42%)

--- src/0062-unique-paths/solution.py
+++ src/0062-unique-paths/solution.py
@@ -1,28 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def bruteForceMemoised(r: int, c: int, rows: int, cols: int, cache: List[List[int]]) -> int:
-            # Do not go out of bounds
-            if r == rows or c == cols:
+        cache = { (m - 1, n - 1): 1 }
+        def dfs(r, c):
+            if r == m or c == n:
                 return 0
-            # means already visited this path
-            if cache[r][c] > 0:
-                return cache[r][c]
-            # we have reached the corner
-            if r == rows - 1 and c == cols - 1:
-                return 1
-            cache[r][c] = bruteForceMemoised(r + 1, c, rows, cols, cache) + bruteForceMemoised(r, c + 1, rows, cols, cache)
-            return cache[r][c]
-
-        def dp(rows: int, cols: int) -> int:
-            prevRow = [0] * cols
-
-            for i in range(rows - 1, -1, -1):
-                currRow = [0] * cols
-                currRow[cols - 1] = 1
-                for j in range(cols - 2, -1, -1):
-                    currRow[j] = currRow[j + 1] + prevRow[j]
-                prevRow = currRow
-            return prevRow[0] 
-
-        return dp(m, n)
-
+            if (r, c) in cache:
+                return cache[(r, c)]
+            cache[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
+            return cache[(r, c)]
+        return dfs(0, 0)
