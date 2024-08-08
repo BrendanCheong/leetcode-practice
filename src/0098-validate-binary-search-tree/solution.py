d0080f60c6985b4d1777f763998d472be7df11fb
Brendan Cheong <brendancej82@gmail.com>
2024-08-08 04:22:18 +0000

    [LeetCode Sync] Runtime - 31 ms (97.33%), Memory - 18.5 MB (12.88%)

--- src/0098-validate-binary-search-tree/solution.py
+++ src/0098-validate-binary-search-tree/solution.py
@@ -1,26 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        """
-        BST is false when the root is not smaller than the right
-        and when the root is not bigger than the left.
-        
-        We traverse the BST by moving through the tree in pre-order DFS traversal
-        by starting of -inf, inf, we see where the number ranges
-        """
-        def validTree(node, left, right):
+        def dfs(node: TreeNode, left: int, right: int):
+            # reached the end so just unravel the recursion
             if not node:
-                return True # empty BST is still a BST
-            if not (node.val < right and node.val > left):
+                return True
+            # a valid BST must be between the values
+            if not (left < node.val < right):
                 return False
-            # move to the left first then to the right
-            # when traversing left, we know the left node and root node
-            # when traversing right, we know the right node and root node
-            return validTree(node.left, left, node.val) and validTree(node.right, node.val, right)
-        return validTree(root, float('-inf'), float('inf'))
-        
+            # go left and right
+            return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
+
+        return dfs(root, float("-inf"), float("inf"))
