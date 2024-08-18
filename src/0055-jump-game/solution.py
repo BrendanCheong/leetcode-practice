b86f4562e0388fdc5f9843af097d184c3d24f3e7
Brendan Cheong <brendancej82@gmail.com>
2024-08-18 04:34:41 +0000

    [LeetCode Sync] Runtime - 343 ms (83.45%), Memory - 17.6 MB (89.70%)

--- src/0055-jump-game/solution.py
+++ src/0055-jump-game/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        """
-        If you want to reach the end, your furthest possible position must
-        be greater than the every index you face!
-        """
-        position = 0
-        for i, jump in enumerate(nums):
-            if i > position:
-                # if the index is greater than the furthest possible position that can be jumped
-                # it means that our furthest position is less than whats required
-                # so we terminate 
+        travel_power = 0
+        for n in nums:
+            if travel_power < 0:
                 return False
-            position = max(position, i + jump)
+            # Greedily take the highest num we can see if we need it
+            elif n > travel_power:
+                travel_power = n
+            travel_power -= 1
         return True
