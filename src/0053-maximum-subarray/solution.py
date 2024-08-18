fda6f3b84ac08844a77cd0baca74c176546f9737
Brendan Cheong <brendancej82@gmail.com>
2024-08-18 03:11:08 +0000

    [LeetCode Sync] Runtime - 525 ms (57.02%), Memory - 31.1 MB (37.77%)

--- src/0053-maximum-subarray/solution.py
+++ src/0053-maximum-subarray/solution.py
@@ -1,32 +1,25 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         """
-        DP memoised recursion tree solution
+        We are gonna use Kadanes greedy algo here.
+        Basically, we reset the sliding window once the currSum < 0.
+        So lets say we are 4, -1, 5, the window is fine.
+        But if its 4, -1, 5, -7, 2, 3, the window is reset at -7 (since the sum is now < 0), we start
+        from the next number after -7.
+
+        We can use greedy here because we want the maximum possible value
+        and negative values don't let us do that
+
+        Time: O(n)
+        Space: O(1)
         """
-        from functools import cache
-        @cache
-        def solve(i, must_pick):
-            # Base Case, if reach end of array, I must decide to end
-            # if I must pick the next num but end of array, pick 0
-            # if no need to pick the next num, just return the smallest possible num 
-            # so it doesn't mess with the max() function 
-            if i >= len(nums):
-                if must_pick:
-                    return 0
-                else:
-                    return float("-inf")
+        max_sum = nums[0]
+        curr_sum = 0
+        for n in nums:
+            curr_sum += n
+            max_sum = max(max_sum, curr_sum)
 
-            # Include current number and go on to the next number
-            # this is the first decision
-            include_current = nums[i] + solve(i + 1, True)
-
-            # Exclude current number (only if must_pick is False)
-            # this is the second decision
-            exclude_current = 0 if must_pick else solve(i + 1, False)
-
-            return max(include_current, exclude_current)
-
-        return solve(0, False)
-        
-
-        
+            # reset window if less than 0, greedy algos want maximum possible number!
+            if curr_sum < 0:
+                curr_sum = 0
+        return max_sum
