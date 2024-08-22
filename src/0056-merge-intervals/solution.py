1b11834bd3c62bf253905d21899cbf16df4e8b78
Brendan Cheong <brendancej82@gmail.com>
2024-08-22 14:21:16 +0000

    [LeetCode Sync] Runtime - 122 ms (63.97%), Memory - 20.5 MB (79.01%)

--- src/0056-merge-intervals/solution.py
+++ src/0056-merge-intervals/solution.py
@@ -1,27 +1,27 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         """
-        Time: O(n log n), we must sort based on start value
-        
+        After sorting, merge overlapping intervals.
+        An interval can be merged if
+        - The start of the next interval is less than or equal to the end of the current interval.
         """
-        START, END = 0, -1
-        intervals = sorted(intervals, key=lambda int: int[START])
-        # usually we can assume that its sorted according to start time, but in this case its not lol.
-        res = []
-        for i, curr_int in enumerate(intervals):
-            prev = res[END] if res else None
-            # edge cases:
-            # new interval goes before all the intervals, no merging
-            # new interval goes after all the intervals, no merging
-            if (i == 0):
-                # edge case where there is only 1 interval
-                res.append(curr_int)
-            elif (curr_int[START] <= prev[END]):
-                # merge when there is an overlap
-                merged_interval = [min(curr_int[START], prev[START]), max(curr_int[END], prev[END])]
-                res.pop() and res.append(merged_interval)
+        # Edge case
+        if not intervals:
+            return []
+
+        # We must sort to get intervals starting from the start timing in O(n log n)
+        intervals.sort(key=lambda x: x[0])
+        res = [intervals[0]]
+
+        # O(n) time to loop through all
+        for i in range(1, len(intervals)):
+            current_interval = res[-1]
+            next_interval = intervals[i]
+            # Check if can merge, then merge
+            if next_interval[0] <= current_interval[-1]:
+                # Note the ends are not sorted order, so we must find highest
+                res[-1] = [current_interval[0], max(next_interval[-1], current_interval[-1])]
             else:
-                # if there is no overlap, just append
-                res.append(curr_int)
+                res.append(intervals[i])
         return res
-        
+
