089e801920c39b9fc21c9846870ae406b2212891
Brendan Cheong <brendancej82@gmail.com>
2024-08-22 15:35:31 +0000

    [LeetCode Sync] Runtime - 68 ms (90.06%), Memory - 19.9 MB (15.36%)

--- src/0057-insert-interval/solution.py
+++ src/0057-insert-interval/solution.py
@@ -1,37 +1,40 @@
-#link: https://leetcode.com/problems/insert-interval/
-
-from typing import List
-
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # intervals = sorted(intervals, key=lambda arr: arr[0])
-        # assume already sorted by the start timings
-        res = []
-        START, END = 0, -1 # start and end of an array is 0, and -1
-        
-        for i, ele in enumerate(intervals):
-            # edge cases:
-            # new interval goes before all the intervals, no merging
-            # new interval goes after all the intervals, no merging
-            if (newInterval[END] < ele[START]):
-                # if new interval end time is before the start time of the current interval,
-                # it means we end earlier, so put it in the front(left side of array)
-                res.append(newInterval)
-                return res + intervals[i:] # new copy of remaining intervals
-            elif (newInterval[START] > ele[END]):
-                # interval start time is bigger than the end time of the current interval,
-                # it means we start way later, so put to the end(right side of array)
-                # don't return, there could be overlap with following intervals
-                res.append(ele)
-            else:
-                # if there is overlap, we need to merge the intervals
-                newInterval = [min(ele[START], newInterval[START]), max(ele[END], newInterval[END])]
-                # its the min of the start time of the current interval and the new interval
-        
-        # if we reach here, means that first if condition never executed
-        # as we are at the end of the array, so we need to append the new interval
-        res.append(newInterval)
+        """
+        There are 3 scenarios
+        1. I just put the new interval first then the next interval
+        2. Merge and combine the intervals
+        3. I put the new interval at the very end
 
+        Time: O(n)
+        Space: O(n)
+        """
+        res = []
+        newInterval_start, newInterval_end = newInterval[0], newInterval[-1]
+
+        for i, interval in enumerate(intervals):
+            curr_start, curr_end = interval[0], interval[-1]
+            # 1. the new interval must be placed before
+            # since its sorted intervals, we can return early
+            if newInterval_end < curr_start:
+                res.append([newInterval_start, newInterval_end])
+                # append rest of the array
+                return res + intervals[i:]
+            # 2. the new interval must be placed at the end
+            # Note: we can't decide to place at the end until we iterated through the whole thing
+            elif newInterval_start > curr_end:
+                res.append(interval)
+            # Merge the intervals
+            else:
+                merged_interval = [
+                    min(newInterval_start, curr_start),
+                    max(newInterval_end, curr_end)
+                ]
+                # Update new interval
+                newInterval_start, newInterval_end = merged_interval[0], merged_interval[-1]
+
+        # Remember now we can decide to add the newInterval at the end
+        res.append([newInterval_start, newInterval_end])
+        
         return res
 
-        
