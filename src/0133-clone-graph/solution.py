31a2811323b7fc350a2499b0ccbbbe6708d1978c
Brendan Cheong <brendancej82@gmail.com>
2024-08-26 08:44:02 +0000

    [LeetCode Sync] Runtime - 43 ms (37.66%), Memory - 16.9 MB (39.58%)

--- src/0133-clone-graph/solution.py
+++ src/0133-clone-graph/solution.py
@@ -1,41 +1,21 @@
-#link: https://leetcode.com/problems/clone-graph/
-from collections import deque
-
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
+from typing import Optional
+
 class Solution:
-    """
-    Since we are using BFS, its a typical O(V+E) time complexity,
-    where we visit all neighbours of all nodes, and connect them to their copies.
-    we can keep track of visited or unconnected nodes using a HashMap in O(1) time
-    """
-    def bfs(self, node):
-        graph = { node: Node(node.val) }
-        q = [node]
+    def cloneGraph(self, node: Optional[Node]) -> Optional[Node]:
+        cache = {}
+        def dfs(node):
+            if node in cache:
+                return cache[node]
+            newNode = Node(node.val)
+            cache[node] = newNode
+            for n in node.neighbors:
+                cache[node].neighbors.append(dfs(n))
+            return newNode
+        return dfs(node) if node else None
 
-        while q:
-            curr = q.pop() # a queue dequeues from head
-            for nei in curr.neighbors:
-                if nei not in graph:
-                    graph[nei] = Node(nei.val)
-                    q.append(nei) # visit unvisited nodes
-                # connect the node copy at hand to its neighboring nodes (also copies)
-                # so connect the current node to the neighbours we just found
-                # AND Connect to neighbours THAT HAVEN'T BEEN CONNECTED YET
-                # NOTE: we must connect the current node in the CLONED GRAPH to the neighbours
-                # not connect curr.neighbors to nei.neighbors
-                graph[curr].neighbors.append(graph[nei])
-        return graph
-
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        # base case, if the node is None, return None
-        if not node:
-            return None
-        cloned_graph = self.bfs(node)
-
-        # return copy of the starting node
-        return cloned_graph[node]
