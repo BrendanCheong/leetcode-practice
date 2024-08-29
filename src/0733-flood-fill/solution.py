36da39f42a9d723886d0e33a6f2966e41d4feb0c
Brendan Cheong <brendancej82@gmail.com>
2024-08-29 06:24:54 +0000

    [LeetCode Sync] Runtime - 60 ms (90.79%), Memory - 17 MB (5.85%)

--- src/0733-flood-fill/solution.py
+++ src/0733-flood-fill/solution.py
@@ -1,27 +1,22 @@
-from typing import *
-from collections import deque
-
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows, cols = len(image), len(image[0])
-        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-        visited, q = set(), deque()
+        ROWS, COLS = len(image), len(image[0])
+        start_color = image[sr][sc]
+        
+        # If the start color is already the new color, no changes are needed
+        if start_color == color:
+            return image
+        
+        def dfs(r: int, c: int) -> None:
+            if (
+                r not in range(ROWS) or
+                c not in range(COLS) or
+                image[r][c] != start_color
+            ):
+                return
+            image[r][c] = color
+            for dr, dc in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                dfs(r + dr, c + dc)
 
-        #0. edge case
-        if not image or not image[0]:
-            return [[]]
-
-        #1. start off from the given cell, cell is valid
-        q.append((sr, sc))
-
-        while q:
-            curr_r, curr_c = q.popleft()
-            if (curr_r, curr_c) not in visited:
-                visited.add((curr_r, curr_c))
-            for x, y in directions:
-                neighbor_r, neighbor_c = curr_r + x, curr_c + y
-                if neighbor_r in range(rows) and neighbor_c in range(cols) and image[neighbor_r][neighbor_c] == image[sr][sc] and (neighbor_r, neighbor_c) not in visited:
-                    image[neighbor_r][neighbor_c] = color
-                    q.append((neighbor_r, neighbor_c))
-        image[sr][sc] = color
+        dfs(sr, sc)
         return image
