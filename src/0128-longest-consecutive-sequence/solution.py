80f73eafe73eba0a7ec73a94df65d30ba472fc90
Brendan Cheong <brendancej82@gmail.com>
2024-08-29 11:26:49 +0000

    [LeetCode Sync] Runtime - 640 ms (38.22%), Memory - 58.9 MB (5.01%)

--- src/0128-longest-consecutive-sequence/solution.py
+++ src/0128-longest-consecutive-sequence/solution.py
@@ -1,68 +1,36 @@
-class UnionFind:
-    def __init__(self):
-        self.parent = {}
-        self.rank = {}
-        self.size = {}  # This keeps track of the size of each connected component
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-                self.size[rootX] += self.size[rootY]
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-                self.size[rootY] += self.size[rootX]
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
-                self.size[rootX] += self.size[rootY]
-
-    def add(self, x):
-        if x not in self.parent:
-            self.parent[x] = x
-            self.rank[x] = 0
-            self.size[x] = 1
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        """
-        Technically you can solve this using UFDS (Union Find). A sequence of numbers is just a set
-        WE need to find the start of the sequence. If Im "0", Im automatically the start.
-        If Im "100", see if theres a "99", if not Im the starting number so I will move on.
-        We can do a while loop to find the next number.
-        """
-        res, num_set = 0, set(nums)
-        for n in nums:
-            if (n - 1) in num_set:
-                # means we are not the start of the sequence and we can ignore
-                continue
-            else:
-                current_length = 0
-                while (n + current_length) in num_set:
-                    current_length += 1
-                res = max(current_length, res)
-        return res
-
-    def longestConsecutiveUnionFindDataStructure(self, nums: List[int]) -> int:
         if not nums:
             return 0
 
-        uf = UnionFind()
+        # Initialize UFDS
+        par = {num: num for num in nums}
+        rank = {num: 1 for num in nums} # start of from 1, cuz means the group has 1 node
+
+        def find(n):
+            if n != par[n]:
+                par[n] = find(par[n])
+            return par[n]
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2] # Add the sizes of the groups together
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+
+        # Union consecutive elements
         for num in nums:
-            uf.add(num)
+            if num - 1 in par:
+                union(num, num - 1)
+            if num + 1 in par:
+                union(num, num + 1)
 
-        for num in nums:
-            if num + 1 in uf.parent:
-                uf.union(num, num + 1)
-
-        # Find the maximum size of the connected components
-        return max(uf.size.values())
-
+        # Find the largest set
+        return max(rank.values())
