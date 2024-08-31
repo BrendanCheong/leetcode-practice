fbae59aabfc66c19960303834eda02cf8eaa6ba9
Brendan Cheong <brendancej82@gmail.com>
2024-08-31 09:29:53 +0000

    [LeetCode Sync] Runtime - 42 ms (86.79%), Memory - 19.3 MB (79.64%)

--- src/0543-diameter-of-binary-tree/solution.py
+++ src/0543-diameter-of-binary-tree/solution.py
@@ -1,4 +1,3 @@
-from typing import *
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,23 +5,20 @@ from typing import *
 #         self.left = left
 #         self.right = right
 class Solution:
-    """
-    Since the diameter can start at any node, we have to find the longest left and right path of each and every node.
-    """
-    def __init__(self):
-        self.max_diameter = 0
-    def depth(self, node: Optional[TreeNode]):
-        # find the depth of the left and right nodes
-        left = self.depth(node.left) if node and node.left else 0
-        right = self.depth(node.right) if node and node.right else 0
-
-        # the diameter is now the longest left and right path combined
-        # use it to find the max diameter
-        current_diameter = left + right
-        self.max_diameter = max(self.max_diameter, current_diameter)
-
-        # calculate the depth of each side left/right
-        return 1 + (left if left > right else right)
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.depth(root)
-        return self.max_diameter
+        diameter = 0
+        def maxDepth(node: Optional[TreeNode]):
+            nonlocal diameter
+            if not node:
+                return 0
+
+            maxDepthLeft = maxDepth(node.left)
+            maxDepthRight = maxDepth(node.right)
+
+            diameter = max(diameter, maxDepthLeft + maxDepthRight)
+
+            return 1 + max(maxDepthLeft, maxDepthRight)
+        
+        maxDepth(root)
+        return diameter
+
