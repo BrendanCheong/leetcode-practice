cc5e1a8d449b8152787f1db0d9bc6978fd36e28f
Brendan Cheong <brendancej82@gmail.com>
2024-09-11 10:19:38 +0000

    [LeetCode Sync] Runtime - 182 ms (13.34%), Memory - 18 MB (21.44%)

--- src/0169-majority-element/solution.py
+++ src/0169-majority-element/solution.py
@@ -1,19 +1,29 @@
-from collections import Counter
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         """
-        O(1) space solution
-        O(n) time
+        *IMPORTANT: The Boyer Moore Algo
+        algorithm works on the fact that if an element occurs more than N/2 times, 
+        it means that the remaining elements other than this would definitely be less than N/2. 
+
+        The key insight is that the majority element, if it exists, will always survive the pairing process 
+        because it appears more than n/2 times. 
+        All other elements can at most cancel out n/2 occurrences of the majority element, 
+        leaving at least one occurrence to be picked as the final candidate.
+
+        Basically, a vote for a candidate increase by 1, a non-vote decrease by 1
+        we will never have 0 votes for the majority candidate
+        
+        Time: O(n)
+        Spac: O(1)
         """
-        c = Counter(nums)
-        n = len(nums)
-        check = n // 2
-        for k, v in c.items():
-            if v > check:
-                return k
-        return 0
-
-
-        
-        
+        candidate = -1
+        votes = 0
+        for i, ele in enumerate(nums):
+            if votes == 0:
+                candidate = ele
+                votes = 1
+            elif ele != candidate:
+                votes -= 1
+            elif ele == candidate:
+                votes += 1
+        return candidate 
