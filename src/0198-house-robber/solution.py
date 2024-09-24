514b808a54417994e775550c8a91c3101aa32b98
Brendan Cheong <brendancej82@gmail.com>
2024-09-24 03:30:37 +0000

    [LeetCode Sync] Runtime - 38 ms (41.80%), Memory - 16.6 MB (6.39%)

--- src/0198-house-robber/solution.py
+++ src/0198-house-robber/solution.py
@@ -1,30 +1,30 @@
+from functools import cache
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         """
-        Consider the case [2,1,1,5] we must create a decision tree of all decisions.
-        So its 2 and 1, 1 and 5 or 2 and 5. Note that we if we brute force we would have
-        duplicate combinations. So we must use DP here!
-        The sub-problem is:
-        rob = max(nums[i] + rob[i + 2 : n], rob[i : n])
-        So its either we take the number we have now plus the previous max value, or the
-        current max value
-        repeated values are found because we computed the previous max before.
+        There are 2 decisions here
+        1. Rob the current house, move on to next non-adjacent house (+2 index)
+        2. Skip current house, move on to adjacent house (+1 index)
+
+        Base case here:
+        1. We must stay within bounds of the array, (0 <= i <= len(arr))
+
+        Return case:
+        1. We want to find the maximum of both decisions
         """
-        # edge cases:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        elif n == 2:
-            return max(nums[0], nums[1])
-        
-        dp = [nums[0], max(nums[0], nums[1])]
-        i = 2
-        while i < n:
-            max_val = max(nums[i] + dp[0], dp[1])
-            dp[0] = dp[1]
-            dp[1] = max_val
-            i += 1
+        @cache
+        def dp(start: int):
+            # we are out of bounds, return 0 to not add anything to the robbery
+            if start >= len(nums):
+                return 0
+            # Decision 1, rob the house we are on and go on to next house (non-adjacent)
+            take = dp(start + 2) + nums[start]
+            # Decision 2, skip the house, move on to adjacent house
+            skip = dp(start + 1)
 
-        return dp[-1]
-
-        
+            # Return case: Find the maximum robbery
+            return max(take, skip)
+    
+        # start from 0, within bounds
+        return dp(0)
