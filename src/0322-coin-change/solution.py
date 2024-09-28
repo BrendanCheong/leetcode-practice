c9e7d4c927c0e692fd2c14d3409622b725291cfc
Brendan Cheong <brendancej82@gmail.com>
2024-09-28 11:36:14 +0000

    [LeetCode Sync] Runtime - 817 ms (49.33%), Memory - 16.8 MB (78.84%)

--- src/0322-coin-change/solution.py
+++ src/0322-coin-change/solution.py
@@ -1,25 +1,47 @@
-from functools import cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def dp(start: int, current_amount: int) -> int:
-            # Base case: If we successfully found the amount needed, we return 0 (go back up the recursion tree)
-            # means we have chosen all the needed coins, amount is 0
-            if current_amount == 0:
-                return 0
-            # Base case: Invalid state, if we ran out of coins to choose from or overshot the amount, mark as invalid state
-            # overshot meaning we took the wrong coin, ran out of coins means reached end of bounds
-            # use infinity here since its min problem, as infinity means we wont choose this combination with min()
-            if start == len(coins) or current_amount < 0:
-                return float('inf')
+        """
+        Bounds:
+            i: 0 - len(coins)
+            x: 0 - amount
 
-            # Decision 1: take the coin at this index, +1 means we found a valid coin to take
-            take = dp(start, current_amount - coins[start]) + 1
+        Order:
+            general term is a minimum of take and skip
 
-            # Decision 2: skip this coin, move on to the next coin to get to amount
-            skip = dp(start + 1, current_amount)
+            take:
+                => i is constant
+                => x - coins[i], note coins[i] > 0
+                => since x - coins[i] < x (always look at change vs original)
+                => small before big
+                => 0 before amount, we solve smaller subproblems
 
-            # End case, we needed fewest combination
-            return min(take, skip)
-        res = dp(0, amount)
-        return -1 if res == float('inf') else res
+            skip:
+                => x is constant
+                => i + 1 > i (change vs original)
+                => big before small
+                => len(coins) before 0, we process larger indices
+        """
+        # Base case: no combination possible, coin collected < amount. We'll just make all impossible for now then update later
+        dp = [float('inf')] * (amount + 1)
+
+        # Base case: 0 coins to make amount 0, x == 0 case
+        dp[0] = 0
+
+        # Loop through amounts and coins
+        for coin in coins:
+            # this serves as our dp[i + 1][x], no need for i + 1 since we start from the coin value
+            for x in range(coin, amount + 1):
+                # Decision 1: take the coin at this index, if the coin value is <= current amount
+                if coin <= x:
+                    take = dp[x - coin] + 1
+                else:
+                    take = float('inf')
+
+                # Decision 2: skip this coin, move on to the next coin to get to amount
+                skip = dp[x]
+
+                # End case, we needed fewest combination
+                dp[x] = min(take, skip)
+
+        # If dp[0][amount] is still infinity, it means we cannot make that amount with the given coins
+        return -1 if dp[amount] == float('inf') else dp[amount]
