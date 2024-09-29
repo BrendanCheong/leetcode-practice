c8446b458e1f8c0a322ea46d8440a8df5708ff02
Brendan Cheong <brendancej82@gmail.com>
2024-09-29 06:06:21 +0000

    [LeetCode Sync] Runtime - 40 ms (16.90%), Memory - 16.6 MB (17.30%)

--- src/0070-climbing-stairs/solution.py
+++ src/0070-climbing-stairs/solution.py
@@ -1,22 +1,20 @@
+from functools import cache
 class Solution:
+    @cache
     def climbStairs(self, n: int) -> int:
-        """
-        This is a DP question, as it requires optimising the recursion tree.
-        The decision we can make is, we can make the left subtree = n - 1 and the right
-        subtree = n - 2.
-
-        We can either do this using 
-        1. 1D DP - memoization with a hashmap (top-down DP), 
-        2. 1D DP - bottom-up approach with 2 values or a small array
-        """
-        if n < 2:
+        if n <= 1:
             return 1
-        else:
-            dp = [1, 1]
-            i = 2
-            while i <= n:
-                next_val = dp[1] + dp[0]
-                dp[0] = dp[1]
-                dp[1] = next_val
-                i += 1
-            return dp[1]
+        # Bounds, 0 to n
+        dp = [0] * (n + 1)
+
+        # edge case, n <= 1 is 1
+        dp[0] = 1
+        dp[1] = 1
+
+        # outer loop, from 2 to n + 1, since we handled base cases
+        for i in range(2, n + 1):
+            # the same as the recursive function (n - 1) + (n - 2)
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        # return the end
+        return dp[n]
