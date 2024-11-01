a489ef1882845d277a2aa9287c0ccd49b5e0d611
Brendan Cheong <brendancej82@gmail.com>
2024-11-01 07:30:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (98.59%)

--- src/0792-binary-search/solution.py
+++ src/0792-binary-search/solution.py
@@ -1,33 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        """
-        Since the array is already sorted, we can use binary search.
-        Note that middle of array is first pointer + (last - first) // 2
-
-        Also note that binary search is about chopping the array into half based
-        on the 2 pointers
-
-        You may find that other binary search qns have different if statements
-        But all evolve around left/right = middle -1/+1
-
-        Time: O(log n)
-        Space: O(1)
-        """
         left, right = 0, len(nums) - 1
-
-        # We use left <= right and not left < right
-        # for edge case when array is length 1, like arr = [5]
         while left <= right:
-            middle = left + ((right - left) // 2)
-            if nums[middle] < target:
-                # we are too small, so we need to go bigger
-                # which is the right hand side
-                left = middle + 1
-            elif nums[middle] > target:
-                # we are too big, so we need go smaller
-                # which is left hand side
-                right = middle - 1
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
             else:
-                return middle
-        # If no answer can be found
+                return mid
         return -1
